1dda51c7c91064404803a46460248e2b0161175e
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-22 13:09:15 +0300

    Testing POST /todos

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-
 var app = express();
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -25,3 +25,5 @@ app.listen(3000, () => {
    console.log('Started on port 3000'); 
 });
 
+
+module.exports = {app};
