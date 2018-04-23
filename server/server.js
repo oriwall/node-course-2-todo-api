19b46cc34dd0f77d2d589ee0bd0b3dcd8fb8f735
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-23 14:58:05 +0300

    added PATCH /todos/:id method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -71,6 +72,33 @@ app.delete('/todos/:id', (req, res) => {
    }); 
 });
 
+
+app.patch('/todos/:id', (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text','completed']);
+    
+   if (!ObjectID.isValid(id)) {
+     return res.status(404).send();
+     //console.log('ID not valid');
+   }
+    
+   if (_.isBoolean(body.completed) && body.completed) {
+       body.completedAt = new Date().getTime();
+   } else {
+       body.completed = false;
+       body.completedAt = null;
+   }
+    
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+       
+        if (!todo) {
+            return res.status(404).send();
+        }       
+      res.send({todo}); 
+   },(e)=>{
+      res.status(400).send(e);
+   }); 
+});
 
 app.listen(port, () => {
    console.log(`Started on port ${port}`); 
