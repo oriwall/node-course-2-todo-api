a6fdf8d4e09ff9b768aa2f036e8a4ad58b360bc9
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-23 10:05:34 +0300

    added delete endpoint

--- server/server.js
+++ server/server.js
@@ -52,6 +52,26 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
+app.delete('/todos/:id', (req, res) => {
+   var id = req.params.id;
+    
+   if (!ObjectID.isValid(id)) {
+     return res.status(404).send();
+     //console.log('ID not valid');
+   }
+    
+   Todo.findByIdAndRemove(id).then((todo)=>{
+       
+        if (!todo) {
+            return res.status(404).send();
+        }       
+      res.send({todo}); 
+   },(e)=>{
+      res.status(400).send(e);
+   }); 
+});
+
+
 app.listen(port, () => {
    console.log(`Started on port ${port}`); 
 });
