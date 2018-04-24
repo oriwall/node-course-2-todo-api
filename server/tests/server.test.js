2f1bd4ce55f138f867ab40bfb8874bcb0c44e7ad
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-24 11:37:19 +0300

    added PATCH /todos/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 },{
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -147,4 +149,39 @@ describe('DELETE /todos/:id', () => {
     
     
     
+});
+
+
+describe('PATCH /todo/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[0]._id.toHexString(); 
+        var text = '****Updated Text****';
+        
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({text, completed:true})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            //expect(res.body.todo.completedAt).toBeA('number');
+        })
+        .end(done);
+    });
+    
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString(); 
+        var text = '****Updated Text****';
+        
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({text, completed:false})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toBe(null);
+        })
+        .end(done);
+    });
 });
