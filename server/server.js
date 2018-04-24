0d9018f0804927f6e7a6e4ba465aa2382d934f03
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-24 14:12:19 +0300

    Added authentication to /users/me

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -118,6 +119,11 @@ app.post('/users', (req, res) => {
     });
 });
 
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 
 app.listen(port, () => {
