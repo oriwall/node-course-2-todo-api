029dab4429dce32d1406476fcb38971dd465e989
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-26 11:57:17 +0300

    Fixed a bug in the configuration

--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
     
     user.tokens = user.tokens.concat([{access, token}]);
     return user.save().then(() => {
