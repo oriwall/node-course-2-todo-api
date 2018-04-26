496c46ea02d7c4503f22e2af2ddb5af6ae36be5a
Ori Wallenstein <ori.wallenstein@learningzone.me>
2018-04-26 10:23:15 +0300

    added test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -272,3 +272,23 @@ describe('POST /users/login', () => {
     });
     
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            User.findByToken(users[0].tokens[0].token).then((user) => {
+                expect(user).toBe(null);
+                done();
+            }).catch((e) => done(e));
+        });
+        
+    });
+});
